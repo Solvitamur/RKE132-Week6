c9617fbb3076d8f6bc8eb591f61ac6123682534d
Solvita <solvitamur@gmail.com>
2022-12-02 21:04:57 +0200

    add week6 code

---
+++ Week6StringsAndArrays/Program.cs
@@ -0,0 +1,30 @@
+﻿string hello = "i wanna go play videogameees";
+
+int stringLength = hello.Length;
+//string trimmedString = hello.Trim();
+
+
+hello = hello.Trim();
+
+char firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+ 
+hello = hello.Replace(hello[0], char.ToUpper(hello[0])).Replace('?', '!');
+
+firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+
+Console.WriteLine(hello);
+
+//int wordCounter = 1;
+//for(int i = 0; i < hello.Length; i++)
+//{
+//    //Console.WriteLine($"{i} symbol in sring: {hello[i]}");
+//    //if(hello[i] == ' ')
+//    if (char.IsWhiteSpace(hello[i])) 
+//    {
+//       wordCounter++;  
+//    }
+//} 
+
+//Console.WriteLine($"Word count in {hello}: {wordCounter} words.");
